37b4b358e82af89684084955a2ab7df3d228563c
Felix Santana <flexsantana99@gmail.com>
2023-05-14 21:23:20 -0400

    Subiendo las clases Bank y TestBanking

--- BankPrj/src/com/mybank/domain/Bank.java
+++ BankPrj/src/com/mybank/domain/Bank.java
@@ -1,25 +1,23 @@
 package com.mybank.domain;
 public class Bank {
-    private static Customer[] customers;
-	private static int numberOfCustomers;
-	
-	static {
-		customers = new Customer[5];
+    private Customer[] customers;
+	private int numberOfCustomers;
+
+	public Bank(){
+		customers = new Customer[10];
 		numberOfCustomers = 0;
 	}
-	
-	private Bank(){}
 
-	public static void addCustomer(String f, String l){
+	public  void addCustomer(String f, String l){
 		int i = numberOfCustomers++;
 		customers[i] = new Customer(f, l);
 	}
 	
-	public static int getNumOfCustomers(){
+	public  int getNumOfCustomers(){
 		return numberOfCustomers;
 	}
 	
-	public static Customer getCustomer(int customer_index){
+	public  Customer getCustomer(int customer_index){
 		return customers[customer_index];
 	}
 }
