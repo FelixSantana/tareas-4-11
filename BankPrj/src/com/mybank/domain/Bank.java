b3e51b16ac9befcfb53ae8659b271d01eddef7d4
Felix Santana <flexsantana99@gmail.com>
2023-05-27 20:38:21 -0400

    Modificando las clases para hacer la unidad 7

--- BankPrj/src/com/mybank/domain/Bank.java
+++ BankPrj/src/com/mybank/domain/Bank.java
@@ -1,23 +1,25 @@
 package com.mybank.domain;
 public class Bank {
-    private Customer[] customers;
-	private int numberOfCustomers;
+    private static Customer[] customers;
+	private static int numberOfCustomers;
 
-	public Bank(){
-		customers = new Customer[10];
+	static {
+		customers = new Customer[5];
 		numberOfCustomers = 0;
 	}
 
-	public  void addCustomer(String f, String l){
+	private Bank(){}
+
+	public static void addCustomer(String f, String l){
 		int i = numberOfCustomers++;
 		customers[i] = new Customer(f, l);
 	}
-	
-	public  int getNumOfCustomers(){
+
+	public static int getNumOfCustomers(){
 		return numberOfCustomers;
 	}
-	
-	public  Customer getCustomer(int customer_index) {
+
+	public static Customer getCustomer(int customer_index){
 		return customers[customer_index];
 	}
 }
